91f535224431dc2736ec2dbe594c5a066e947da1
Inés Pastor-Marazuela <inespm@MacBook-Pro.local>
2026-09-14 23:35:55 +0200

    Migrate website to Astrofy

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Inés Pastor-Marazuela';
+export const SITE_DESCRIPTION = 'Postdoctoral researcher in astrophysics';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
